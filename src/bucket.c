669e5860d6157fd54dc4648738ac92914b0444f0
Sergey Poznyakoff <gray@gnu.org>
2021-11-12 13:50:08 +0200

    Bugfixes
    * src/bucket.c (log2i): Round up to the next hihgest power of 2.
    (_gdbm_cache_init): Set minimal cache size.

--- src/bucket.c
+++ src/bucket.c
@@ -52,27 +52,6 @@ set_cache_entry (GDBM_FILE dbf, cache_elem *elem)
 
 /* Bucket cache table functions */
 
-/*
- * Compute log2(v) using De Brujin sequences.
- * See http://supertech.csail.mit.edu/papers/debruijn.pdf
- */
-static unsigned
-log2i (unsigned v)
-{
-  static const int dbp[32] =
-    {
-      0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
-      8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
-    };
-
-  v |= v >> 1;
-  v |= v >> 2;
-  v |= v >> 4;
-  v |= v >> 8;
-  v |= v >> 16;
-  return dbp[(uint32_t)(v * 0x07C4ACDDU) >> 27];
-}
-
 /* Hash an off_t word into an index of width NBITS. */
 static size_t
 adrhash (off_t adr, size_t nbits)
@@ -615,6 +594,30 @@ _gdbm_write_bucket (GDBM_FILE dbf, cache_elem *ca_entry)
 
 /* Cache manipulation functions. */
 
+/*
+ * Round up V to the next highest power of 2 and compute log2 of
+ * it using De Brujin sequences.
+ * See http://supertech.csail.mit.edu/papers/debruijn.pdf
+ */
+static unsigned
+log2i (unsigned v)
+{
+  static const int dbp[32] =
+    {
+      0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 
+      31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+    };
+
+  v--;
+  v |= v >> 1;
+  v |= v >> 2;
+  v |= v >> 4;
+  v |= v >> 8;
+  v |= v >> 16;
+  v++;
+  return dbp[(uint32_t)(v * 0x077CB531U) >> 27];
+}
+
 /* Initialize the bucket cache. */
 int
 _gdbm_cache_init (GDBM_FILE dbf, size_t size)
@@ -637,7 +640,7 @@ _gdbm_cache_init (GDBM_FILE dbf, size_t size)
   else
     {
       cache_auto = FALSE;
-      bits = log2i (size + 1);
+      bits = log2i (size < 4 ? 4 : size);
       size = 1 << bits;
     }
   
